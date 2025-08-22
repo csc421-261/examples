dd2636b6779fc21c6588d1c6f7bed54d12fb815c
ojo <burt@cs.miami.edu>
2025-08-22 00:31:08 +0000

    ok

---
+++ hello/hello.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/*
+ * hello.c
+ * author: bjr
+ * last-update:
+ *     21 aug 2025 -bjr:
+ *
+ */
+
+int main(int argc, char * arg[]) {
+	printf("hello world!\n") ;
+	return 0 ;
+}
+
