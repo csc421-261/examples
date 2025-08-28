8d389e6132047f9e6aabd5d9458b0a5f0cb81541
Burt Rosenberg <burt@cs.miami.edu>
2025-08-28 18:51:10 -0400

    ok

--- sum-make/sum.c
+++ sum-make/sum.c
@@ -7,12 +7,10 @@
  
 #include <stdio.h>
 #include <stdlib.h>
-#include <assert.h>
 
 #include "sum_util.h"
 
 int main(int argc, char * argv[]) {
-    assert(argc==3) ;
     int a = atoi(argv[1]) ;
     int b = atoi(argv[2]) ;
     printf("%d plus %d is %d\n", a, b, sum(a,b)) ;
